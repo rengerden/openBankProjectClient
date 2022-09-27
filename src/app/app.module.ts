38b0e7bac0ca4eac8c07f7a84e889c568853f037
Miguel Puentes <mpuentes2611@gmail.com>
2022-09-27 17:29:46 -0500

    Agregar form a consent.component.html agregar form builder a conset.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule ,ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +15,7 @@ import { ConsentComponent } from './consent/consent.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/consent/consent.component.ts
+++ src/app/consent/consent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consent',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  banks = ['City', 'HSBC' ,'Banamex'];
+  permissions = [
+    {name: 'ReadAccountsMexicoGroup', value: 'readAccountsMexicoGroup'},
+    {name: 'ReadBalancesMexicoGroup', value: 'readBalancesMexicoGroup'},
+    {name: 'ReadTransactionsMexicoGroup', value: 'readTransactionsMexicoGroup'},
+  ];
+
+  consentForm = this.fb.group({
+    selectBank: ['', [Validators.required]],
+    permissions: this.fb.array([]),
+    frequencyPerDay: ['', [Validators.required]],
+    expirationDateTime: ['', [Validators.required]],
+    recurringIndicator: [false],
+  });
+
+  get selectedBank() {
+    return this.consentForm.get('selectBank');
+  }
+
+  changeBank(e : any) {
+    this.selectedBank?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+
+  onSubmit() {}
 
 }
