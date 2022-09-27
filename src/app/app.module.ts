1e0ab3ab1cfa999ecde8e9c434eb5739201d56e7
Miguel Puentes <mpuentes2611@gmail.com>
2022-09-27 16:04:42 -0500

    Crear componente consent e importar formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsentComponent } from './consent/consent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
