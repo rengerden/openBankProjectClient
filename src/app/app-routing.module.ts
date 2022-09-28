ecf119d97870035debd056299bf7b5b502216224
silverdraco <quimera475@gmail.com>
2022-09-28 16:48:56 -0500

    Maquetado
    Maquetación y corrección de estilos.
    Se crearon variables para datos prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListConsentComponent } from './list-consent/list-consent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list-consent', component: ListConsentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/list-consent/list-consent.component.ts
+++ src/app/list-consent/list-consent.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListConsentComponent implements OnInit {
 
+  apiStandard :string = 'UKOpenBanking';//BerlinGroup  UKOpenBanking
+
+  user = {
+    username: 'silverdrao',
+    email: 'prueba@prueba.com'
+  }
+
+
+  consentId = '';
+  consentStatus = '';
+  frequencyPerDay = '';
+  recurringIndicator = '';
+  validUntil = '';
+
+
+
+  zson = '{"glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}}';
+  account = {
+    AccountId: ''
+  }
+
+  getDataAccount(type: string){
+    const AccountId = this.account.AccountId;
+
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
