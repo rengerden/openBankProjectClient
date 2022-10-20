1b66d227127d40abd83ee295bd7ac6ac79333288
Mike2611 <mpuentes2611@gmail.com>
2022-10-20 11:25:26 -0500

    Crear componente register-consumer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterConsumerComponent } from './register-consumer/register-consumer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterConsumerComponent
   ],
   imports: [
     BrowserModule,
